d629719f9a4c2cfa0144fcdd4b8fb5b43801aaef
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2021-04-08 13:05:02 -0300

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Page404 = (props: Props) => {
+  return (
+    <div>
+      Page Not Found!
+    </div>
+  )
+}
+
+export default Page404
